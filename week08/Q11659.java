ef89e51925946deb61024a37c27e5783f3308f77
GDana <danmi2857@naver.com>
2024-03-02 15:25:18 +0900

    Update: 구간 합 구하기4

---
+++ week08/Q11659.java
@@ -0,0 +1,45 @@
+package week08;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/** [페어프로그래밍] 김현선(N), 장진홍(D)
+ * 구간 합 구하기 4
+ * https://www.acmicpc.net/problem/11659
+ */
+public class Q11659 {
+    public static void main(String[] args) {
+        /*
+         * 메모리 260252KB
+         * 시간 2096ms
+         * 코드길이 779B
+         * */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] num = new int[n];
+        int[] sum = new int[n+1];
+        int[] re = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        sum[0] = 0;
+        for(int i=1; i<=n; i++) {
+            sum[i] = sum[i-1] + num[i-1];
+        }
+
+        for(int i=0; i<m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int s = a == 1 ? 0 : sum[a-1];
+            re[i] = sum[b] - s;
+        }
+
+        for (int i : re) {
+            System.out.println(i);
+        }
+    }
+}
